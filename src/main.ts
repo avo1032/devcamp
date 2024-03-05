0d70c552c73cd585214381529b57eed348e0fdc2
유승완 <swan1032@naver.com>
2024-03-05 22:30:29 +0900

    fix: main.ts 수정

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { setSwagger } from './common/config/swagger';
-import { HttpExceptionFilter } from './common/filter/http.exception.filter';
+import { HttpExceptionFilter } from './common/filters/http.exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
